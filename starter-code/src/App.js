81c64e4a9b87627cb1767c9c10189fe0ea895ea6
Shahriyar <shahryar102@yahoo.com>
2019-11-15 09:18:43 -0500

    final

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,38 +1,169 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bulma/css/bulma.css';
-import foods from './foods.json'
-import FoodBox from './FoodBox.js'
-
+import foodsJson from './foods.json'
+import FoodBox from './FoodBox'
+console.log(foodsJson)
 class App extends Component {
-  displayFood(){
-    let foodList=[...foods]
-    let newFoodList=foodList.splice(0,5);
-    return(
-      newFoodList.map((eachFood,i)=>{
-        return(
-          <FoodBox name={eachFood.name} cal={eachFood.calories} quantity={eachFood.quantity} image={eachFood.image}/>
-        )
-      }
-        
-      )
-    )
+
+  state = {
+    foods: foodsJson,
+    foodToAdd: '',
+    calsToAdd: 0,
+    showForm: false,
+    search: '',
+    listOfFoods: {
+        // "Pizza":{
+        //   quantity: 5,
+        //   calories: 500
+        // },
+        // "Salad": {
+        //   quantity: 3,
+        //   calories: 20
+        // },
+        // "Kiwi" : {
+        //   quantity: 6,
+        //   calories: 99
+        // }
+      } 
   }
+
+  showTodaysFood = () => {
+    let foods = this.state.listOfFoods
+    let array = [] 
+    let total = 0; 
+    for(let key in foods) {
+       console.log( Number(foods[key].calories) )
+       total += Number(foods[key].quantity) * Number(foods[key].calories)
+        array.push (
+            <li key={key}>
+              name: {key} 
+              -
+              calories: {Number(foods[key].quantity) * Number(foods[key].calories)}
+            </li>
+        )
+    }
+    return <ul>TOTAL is {total}!!!!<br></br> {array}</ul>
+  }
+
+
+
+
+
+
+
+  showFoods = () => {
+    return this.state.foods.map((eachFood,i)=>{
+      return < FoodBox updateFoodList={this.updateFoodList} changeQuantity={this.changeTheQuantity} key={i} {...eachFood}/>
+    })
+  }
+
+
+  updateFoodList = (foodBoxState) => {
+    console.log(foodBoxState)
+    let newListOfFoods = {...this.state.listOfFoods}
+    newListOfFoods[foodBoxState.name] = foodBoxState //newListOfFoods['pizza'] = {  caloies: 5, qunatity: 7} 
+    this.setState({
+      listOfFoods: newListOfFoods
+    })
+  }
+
+  addFood = (e) => {
+    e.preventDefault() //e.target.children[0].value -- alternative 
+
+
+    let newFoods = [...this.state.foods] //Copy of all the previous food
+
+
+    let newFood = {
+      name: this.state.foodToAdd,
+      calories: this.state.caloriesToAdd,
+      quantity: 0,
+      image: ''
+    }
+
+    newFoods.unshift(newFood) //I added my new food to the list.  
+
+    this.setState({
+      foods:newFoods,
+      foodToAdd: '',
+      calsToAdd: 0
+    })
+
+    this.setShowForm()
+  }
+
+  setInputOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value //food: 'pizza' 
+    })
+  }
+
+  setShowForm = () => {
+    this.setState({
+      showForm:!this.state.showForm
+    })
+  }
+
+  setSearch = (e) => {
+
+    let filteredFoods = foodsJson.filter(eachFood=>{
+      return eachFood.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+
+
+    this.setState({
+      'search': e.target.value, //search: 'pizza' 
+      foods:filteredFoods
+    })
+
+
+    console.log(filteredFoods)
+  }
+
+ 
+ 
+  showTheForm = () => {
+  
+    if( this.state.showForm ) {
+      return (
+        <form onSubmit={this.addFood}>
+          <input onChange={this.setInputOnChange} placeholder="add a food" name="foodToAdd" type="text"/>
+          <input onChange={this.setInputOnChange} placeholder="how many cals" name="caloriesToAdd" type="number"/>
+
+          <input type="submit" value="Add Food" />
+        </form>
+      )
+    }
+    else{
+      return <button onClick={this.setShowForm}>Show Form</button>
+    }
+    
+  }
+
   render() {
     return (
+
+      <React.Fragment>
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React {Math.random()}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <FoodBox name="vegan pizza" cal="350" quantity="2" image="https://i.imgur.com/DupGBz5.jpg"/> */}
-        {this.displayFood()}
+      <input type="text" name="search" placeholder="Search for a food" onChange={this.setSearch}/>
+      <br></br>
+      {this.showTheForm()}
+
+      {this.showFoods()}
       </div>
-    );
+
+      <div className="todaysFood">
+            Todays Food
+            {this.showTodaysFood()}
+      </div>
+
+      </React.Fragment>    );
   }
 }
 
--- starter-code/src/FoodBox.js
+++ starter-code/src/FoodBox.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react';
-import 'bulma/css/bulma.css';
-import foods from './foods.json'
-
+import React, { Component } from "react";
 
 class FoodBox extends Component {
-    render(){
-        return (
-            <div className="box">
-            <article className="media">
-              <div className="media-left">
-                <figure className="image is-64x64">
-                  <img src={this.props.image} />
-                </figure>
-              </div>
-              <div className="media-content">
-                <div className="content">
-                  <p>
-                    <strong>{this.props.name}</strong> <br />
-                    <small>{this.props.cal}</small>
-                  </p>
-                </div>
-              </div>
-              <div className="media-right">
-                <div className="field has-addons">
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="number" 
-                      value={this.props.quantity}
-                    />
-                  </div>
-                  <div className="control">
-                    <button className="button is-info">
-                      +
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </div>
-        );
 
-    }
-  
+  state = { ...this.props }
+
+  changeTheQuantity = (e) => {
     
-  
+    this.setState({
+      'quantity': e.target.value,
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="box">
+        {Math.random()}
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.image} />
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.calories} cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input className="input" type="number" name={this.props.name} defaultValue={this.props.quantity} onChange={this.changeTheQuantity}/>
+              </div>
+              <div className="control">
+                <button className="button is-info" onClick={(e) => this.props.updateFoodList(this.state)} >+</button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  }
 }
 
 export default FoodBox;
